5ef5280d7a98f676f653a701495dff71af1e24f4
Mojtaba-Gholizadeh <mojtaba.leoooo@gmail.com>
2024-11-06 23:45:53 +0330

    table created

--- src/components/courses/Table.jsx
+++ src/components/courses/Table.jsx
@@ -2,11 +2,14 @@ import '@styles/react/libs/react-select/_react-select.scss'
 import ReactPaginate from 'react-paginate'
 import {useQuery} from '@tanstack/react-query'
 import {getAllCourses} from '../../@core/services/api/courses'
-import {useState} from 'react'
+import {useRef, useState} from 'react'
 import DataTable from 'react-data-table-component'
-import {Card} from 'reactstrap'
+import {Button, Card, Col, Input, Row} from 'reactstrap'
+import {Columns} from './TableColumns'
+import {ChevronDown} from 'react-feather'
 
 export function Table() {
+    const timeout = useRef(null)
     const [params, setParams] = useState({PageNumber: 1, RowsOfPage: 10, Query: null})
 
     const {data: courses} = useQuery({
@@ -15,7 +18,94 @@ export function Table() {
     })
 
     const handlePagination = page => {
-        setParams(prevState => ({...prevState, pageNumber: page}))
+        setParams(prevState => ({...prevState, PageNumber: page.selected + 1}))
+    }
+
+    const handleSearch = val => {
+        console.log(val)
+        if (timeout.current) {
+            clearTimeout(timeout.current)
+        }
+
+        timeout.current = setTimeout(() => {
+            setParams(prevState => ({...prevState, Query: val}))
+            timeout.current = null
+        }, 1000)
+    }
+
+    /* const dataToRender = () => {
+        const filters = {
+            role: currentRole.value,
+            currentPlan: currentPlan.value,
+            status: currentStatus.value,
+            q: searchTerm,
+        }
+         const isFiltered = Object.keys(filters).some(function (k) {
+            return filters[k].length > 0
+        })
+        if (store.data.length > 0) {
+            return store.data
+        } else if (store.data.length === 0 && isFiltered) {
+            return []
+        } else {
+            return store.allData.slice(0, rowsPerPage)
+        }
+    } */
+
+    const handlePerPage = e => {
+        const value = parseInt(e.currentTarget.value)
+        setParams(prevState => ({...prevState, RowsOfPage: value}))
+    }
+
+    const CustomHeader = ({handlePerPage, RowsOfPage, handleSearch, searchTerm}) => {
+        return (
+            <div className="invoice-list-table-header w-100 me-1 ms-50 mt-2 mb-75">
+                <Row>
+                    <Col
+                        xl="6"
+                        className="d-flex align-items-sm-center justify-content-xl-start justify-content-start flex-xl-nowrap flex-wrap flex-sm-row flex-column pe-xl-1 p-0 mt-xl-0 mt-1"
+                    >
+                        <div className="d-flex align-items-center mb-sm-0 mb-1 me-1">
+                            <label className="mb-0" htmlFor="search-invoice">
+                                جستوجو:
+                            </label>
+                            <Input
+                                id="search-invoice"
+                                className="ms-50 w-100"
+                                type="text"
+                                value={searchTerm}
+                                onChange={e => handleSearch(e.target.value)}
+                            />
+                        </div>
+                    </Col>
+                    <Col xl="6" className="d-flex gap-5 align-items-center justify-content-end p-0">
+                        <div className="d-flex align-items-center ww-100 ">
+                            <label htmlFor="rows-per-page">تعداد</label>
+                            <Input
+                                className="mx-50"
+                                type="select"
+                                id="rows-per-page"
+                                value={RowsOfPage}
+                                onChange={handlePerPage}
+                                style={{width: '5rem'}}
+                            >
+                                <option value="10">10</option>
+                                <option value="25">25</option>
+                                <option value="50">50</option>
+                            </Input>
+                            <label htmlFor="rows-per-page">دوره در صفحه</label>
+                        </div>
+
+                        <Button
+                            className="add-new-user"
+                            color="primary" /* onClick={toggleSidebar} */
+                        >
+                            ایجاد دوره جدید
+                        </Button>
+                    </Col>
+                </Row>
+            </div>
+        )
     }
 
     const CustomPagination = () => {
@@ -35,7 +125,7 @@ export function Table() {
                 previousClassName={'page-item prev'}
                 previousLinkClassName={'page-link'}
                 pageLinkClassName={'page-link'}
-                containerClassName={'pagination react-paginate justify-content-end my-2 pe-1'}
+                containerClassName={'pagination react-paginate justify-content-center my-2 pe-1'}
             />
         )
     }
@@ -50,20 +140,20 @@ export function Table() {
                     pagination
                     responsive
                     paginationServer
-                    columns={columns}
+                    columns={Columns}
                     // onSort={handleSort}
-                    // sortIcon={<ChevronDown />}
+                    sortIcon={<ChevronDown />}
                     className="react-dataTable"
                     paginationComponent={CustomPagination}
-                    data={dataToRender()}
+                    data={/* dataToRender() */ courses?.courseDtos}
                     subHeaderComponent={
                         <CustomHeader
-                            store={store}
-                            searchTerm={searchTerm}
-                            rowsPerPage={rowsPerPage}
-                            handleFilter={handleFilter}
+                            // store={store}
+                            searchTerm={params.Query}
+                            RowsOfPage={params.RowsOfPage}
+                            handleSearch={handleSearch}
                             handlePerPage={handlePerPage}
-                            toggleSidebar={toggleSidebar}
+                            // toggleSidebar={toggleSidebar}
                         />
                     }
                 />
--- src/components/courses/TableColumns.jsx
+++ src/components/courses/TableColumns.jsx
@@ -1,16 +1,160 @@
 import Avatar from '@components/avatar'
+import {Archive, FileText, MoreVertical, Trash2} from 'react-feather'
+import {Link} from 'react-router-dom'
+import {Badge, DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown} from 'reactstrap'
 
-const renderCourse = row => {
-    if (row.avatar.length) {
-        return <Avatar className="me-1" img={row.avatar} width="32" height="32" />
-    } else {
+const renderCourseAvatar = row => {
+    /*if (row.tumbImageAddress) {
         return (
             <Avatar
-                initials
-                className="me-1"
-                color={row.avatarColor || 'light-primary'}
-                content={row.fullName || 'John Doe'}
+                className="me-1 overflow-hidden"
+                img={row.tumbImageAddress}
+                width="32"
+                height="32"
             />
         )
-    }
+    }  else {
+        return <Avatar initials className="me-1" color="light-primary" content={row.title} />
+    } */
+    return (
+        <Avatar
+            className="me-1 overflow-hidden"
+            img={row.tumbImageAddress}
+            width="32"
+            height="32"
+        />
+    )
 }
+
+/* const renderRole = row => {
+    const roleObj = {
+        subscriber: {
+            class: 'text-primary',
+            icon: User,
+        },
+        maintainer: {
+            class: 'text-success',
+            icon: Database,
+        },
+        editor: {
+            class: 'text-info',
+            icon: Edit2,
+        },
+        author: {
+            class: 'text-warning',
+            icon: Settings,
+        },
+        admin: {
+            class: 'text-danger',
+            icon: Slack,
+        },
+    }
+
+    const Icon = roleObj[row.role] ? roleObj[row.role].icon : Edit2
+
+    return (
+        <span className="text-truncate text-capitalize align-middle">
+            <Icon
+                size={18}
+                className={`${roleObj[row.role] ? roleObj[row.role].class : ''} me-50`}
+            />
+            {row.role}
+        </span>
+    )
+} */
+
+function colorSelector(status) {
+    if (status === 'شروع ثبت نام') return 'light-success'
+    else if (status === 'منقضی شده') return 'light-danger'
+    else if (status === 'درحال برگزاری') return 'light-warning'
+}
+
+export const Columns = [
+    {
+        name: 'دوره',
+        sortable: true,
+        minWidth: '300px',
+        sortField: 'title',
+        selector: row => row.title,
+        cell: row => (
+            <div className="d-flex justify-content-left align-items-center">
+                {renderCourseAvatar(row)}
+                <div className="d-flex flex-column">
+                    <Link
+                        // to={`/apps/user/view/${row.id}`}
+                        className="user_name text-truncate text-body"
+                        // onClick={() => store.dispatch(getUser(row.id))}
+                    >
+                        <span className="fw-bolder">{row.title}</span>
+                    </Link>
+                    <small className="text-truncate text-muted mb-0">{row.levelName}</small>
+                </div>
+            </div>
+        ),
+    },
+    {
+        name: 'نوع کلاس',
+        sortable: true,
+        minWidth: '172px',
+        sortField: 'typeName',
+        selector: row => row.typeName,
+        cell: row => <span className="">{row.typeName}</span>,
+    },
+    {
+        name: 'وضعیت',
+        minWidth: '138px',
+        sortable: true,
+        sortField: 'statusName',
+        selector: row => row.statusName,
+        cell: row => (
+            <Badge className="text-capitalize" color={colorSelector(row.statusName)} pill>
+                {row.statusName}
+            </Badge>
+        ),
+    },
+    {
+        name: 'سایر',
+        minWidth: '100px',
+        cell: row => (
+            <div className="column-action">
+                <UncontrolledDropdown>
+                    <DropdownToggle tag="div" className="btn btn-sm">
+                        <MoreVertical size={14} className="cursor-pointer" />
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem
+                            tag={Link}
+                            className="w-100"
+                            to={`/apps/user/view/${row.id}`}
+                            // onClick={() => store.dispatch(getUser(row.id))}
+                        >
+                            <FileText size={14} className="me-50" />
+                            <span className="align-middle">Details</span>
+                        </DropdownItem>
+                        <DropdownItem
+                            tag="a"
+                            href="/"
+                            className="w-100"
+                            onClick={e => e.preventDefault()}
+                        >
+                            <Archive size={14} className="me-50" />
+                            <span className="align-middle">Edit</span>
+                        </DropdownItem>
+                        <DropdownItem
+                            tag="a"
+                            href="/"
+                            className="w-100"
+                            onClick={e => {
+                                e.preventDefault()
+                                // store.dispatch(deleteUser(row.id))
+                            }}
+                        >
+                            <Trash2 size={14} className="me-50" />
+                            <span className="align-middle">Delete</span>
+                        </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+            </div>
+        ),
+    },
+]
